9f4d1e3a4539694b16a88c93dcd295a1ae29552e
Tony Xing <tonyxwz@yahoo.com>
2017-11-30 01:33:47 +0100

    Part B: ex 2 3 py + ipynb

--- Module3/PartB/Exercise_B2B3.py
+++ Module3/PartB/Exercise_B2B3.py
@@ -16,7 +16,7 @@ def read_MR_txt(path='fructoseanaerobicfluxgraph.txt'):
     lines = txt.split('\n')
     nl = []
     el = []
-    
+
     for line in lines:
         ll = line.split(';')
         if len(ll)==1:
@@ -48,14 +48,14 @@ def draw_MR_Graph(nl, el):
     nodes['M'] = mNodes
 
     nodeColor = {'R':'#0DFF0D', 'M':'#FF0D0D'}
-    
+
     for e in el:
         if e[0][0] == 'M' and e[1][0] == 'R':
             direction = 'reactant' # M -> R
         else:
             direction = 'product' # R -> M
 
-        G.add_edge(e[0], e[1], 
+        G.add_edge(e[0], e[1],
                    weight=float(e[2]),
                    direction=direction)
 
@@ -86,20 +86,20 @@ def draw_MR_Graph(nl, el):
     return G
 
 
-def my_dijkstra(source, G, weight=1):    
+def my_dijkstra(source, G, weight=1):
     # initialize Green and Red set
     Green = [source]
     Red = list(G.nodes())
     Red.remove(source)
     dist = {}
-    
+
     for n in Red:
         #  infinity
         dist[n] = float('inf')
     dist[source] = 0
 
     while Red:
-#         min_dist = float('inf')
+        # min_dist = float('inf')
         for n in G.neighbors(Green[-1]):
             if n in Red:
                 if isinstance(weight, int):
@@ -107,15 +107,15 @@ def my_dijkstra(source, G, weight=1):
                 elif isinstance(weight, str):
                     edge_data = G.get_edge_data(Green[-1], n)
                     cost = edge_data[weight]
-                
+
                 if dist[n] > dist[Green[-1]] + cost:
                     dist[n] = dist[Green[-1]] + cost
-                
+
         x = min(Red, key=lambda x: dist[x])
         # choose minimal in Red set
         Green.append(x)
         Red.remove(x)
-    
+
     return dist
 
 
@@ -131,11 +131,11 @@ def degree_stats(G):
             degree_distr[degree] = 1
         total_degree += len(t[1])
         numN += 1
-    
+
     draw_bar(degree_distr)
     mean_degree = float(total_degree) / numN
     print "Average degree", mean_degree
-    
+
     # calculate variance
     var = 0.0
     for t in G.adjacency():
@@ -143,7 +143,7 @@ def degree_stats(G):
         var += (degree - mean_degree)**2
     var = var / numN
     print "variance:", var
-    
+
     adj = G.adj
     s = sorted(adj, key=lambda x: len(adj[x]))
     hubs = list()
@@ -152,8 +152,8 @@ def degree_stats(G):
     print hubs
     winner = hubs[len(hubs)-1]
     print "Node", winner[0], "has the greatest degree:", winner[1]
-    
-    
+
+
 def draw_bar(distr):
     N = max(distr.keys()) + 1
     for i in xrange(N):
@@ -170,7 +170,6 @@ def draw_bar(distr):
     ax.set_xlabel("degree")
     ax.set_title("Degree Distribution")
     plt.show()
-    
 
 
 def compartment_analysis(G):
@@ -194,13 +193,13 @@ def compartment_analysis(G):
             freq_table[compartment].append(node)
         else:
             freq_table[compartment] = [node]
-            
+
     fig, ax = plt.subplots()
     ind = np.arange(len(freq_table))
     rects = ax.bar(ind, [len(x) for x in freq_table.values()], 0.5, color="r")
     ax.set_xticks(ind)
     ax.set_xticklabels(freq_table.keys())
-    
+
     for rect in rects:
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
@@ -209,12 +208,12 @@ def compartment_analysis(G):
     ax.set_ylabel("number of R/M")
     ax.set_xlabel("compartment")
     ax.set_title("Compartment analysis")
-    
+
     return freq_table
 
 
 if __name__ = '__main__':
-    # exercise B.2 
+    # exercise B.2
     txt = "fructoseanaerobicfluxgraph.txt"
     nl, el = read_MR_txt(path=txt)
     G = draw_MR_Graph(nl, el)
@@ -242,11 +241,10 @@ if __name__ = '__main__':
     degree_stats(G)
 
     # exercise B.3 frequency table
-    freq_table = compartment_analysis(G)     
+    freq_table = compartment_analysis(G)
     node_num = 0
     for k,v in freq_table.items():
         print k,":", v, "\n"
         node_num += len(v)
-        
-    print node_num == len(G.nodes())
 
+    print node_num == len(G.nodes())
